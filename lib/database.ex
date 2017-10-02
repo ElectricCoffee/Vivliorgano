a47e8b9f60a08e3c482f39bbcf8ef518b1d6bbef
electric coffee <slench102@gmail.com>
2017-10-02 10:32:37 +0200

    add database.ex

---
+++ lib/database.ex
@@ -0,0 +1,6 @@
+defmodule Viv.DB do
+  @moduledoc """
+  DB is a binding to a database.
+  For the sake of my own sanity, this is a SQLite binding, but you can change it to anything you like.
+  """
+end
